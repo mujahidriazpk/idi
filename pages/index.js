931ce75dfd4c47ca0c4a443633ef3773ce02cba8
Mujahid <mujahid@grossiweb.com>
2024-06-20 16:56:56 +0500

    index page design update

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ import files from "../public/img/files.png";
 import bird from "../public/img/bird.png";
 import hands from "../public/img/hands.png";
 import lock from "../public/img/lock.png";
+import doc from "../public/img/doc_scan.png";
+import book from "../public/img/book_scan.png";
+import news from "../public/img/news_scan.png";
+import flim from "../public/img/flim_scan.png";
 import Head from "next/head";
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
@@ -157,8 +161,8 @@ const Home = () => {
         </div>
       </Container>
       <Container className="py-0 pb-8">
-        <div className="flex gap-10  justify-center">
-          <div className="w-1/3">
+        <div className="lg:flex gap-10  justify-center">
+          <div className="mb-14 lg:my-0 w-full lg:w-1/3">
             <div className="flex flex-col justify-between text-center w-full h-full bg-gray-100 px-14 py-14">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 m-auto inline-block text-red-600">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3" />
@@ -169,7 +173,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="w-1/3">
+          <div className="w-full lg:w-1/3">
             <div className="flex flex-col justify-between text-center w-full h-full bg-gray-100 px-14 py-14">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 m-auto inline-block text-red-600">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
@@ -242,37 +246,45 @@ const Home = () => {
       <div className="flex w-full flex-col text-center my-4 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-3xl">
         Additional services
         </div>
-        <div className="grid gap-24 lg:grid-cols-2 xl:grid-cols-4 items-center">
+        <div className="grid gap-0 w-2/3 justify-center m-auto lg:grid-cols-2 xl:grid-cols-4 items-center">
           <div className="flex-row">
-            <div className="flex rounded-full bg-[#2164A1] aspect-w-16 aspect-h-16 h-60 m-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-36 h-36 flex justify-center m-auto text-white">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-              </svg>
-            </div>
+          <Image
+                src={doc}
+                alt="Document Scanning Images"
+                loading="eager"
+                placeholder="blur"
+                className="m-auto"
+              />
             <div className="text-center w-full text-base font-semibold py-2">Document Scanning</div>
           </div>
           <div className="flex-row">
-            <div className="flex rounded-full bg-[#2164A1] aspect-w-16 aspect-h-16 m-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-36 h-36 flex justify-center m-auto text-white">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
-              </svg>
-            </div>
+          <Image
+                src={book}
+                alt="Book Scanning Images"
+                loading="eager"
+                placeholder="blur"
+                className="m-auto"
+              />
             <div className="text-center w-full text-base font-semibold py-2">Book Scanning</div>
           </div>
           <div className="flex-row">
-            <div className="flex rounded-full bg-[#2164A1] aspect-w-16 aspect-h-16 m-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-36 h-36 flex justify-center m-auto text-white">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
-              </svg>
-            </div>
+          <Image
+                src={news}
+                alt="Newspaper Scanning Images"
+                loading="eager"
+                placeholder="blur"
+                className="m-auto"
+              />
             <div className="text-center w-full text-base font-semibold py-2">Newspaper Scanning</div>
           </div>
           <div className="flex-row">
-            <div className="flex rounded-full bg-[#2164A1] aspect-w-16 aspect-h-16 m-auto">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-36 h-36 flex justify-center m-auto text-white">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 0 1-1.125-1.125M3.375 19.5h1.5C5.496 19.5 6 18.996 6 18.375m-3.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-1.5A1.125 1.125 0 0 1 18 18.375M20.625 4.5H3.375m17.25 0c.621 0 1.125.504 1.125 1.125M20.625 4.5h-1.5C18.504 4.5 18 5.004 18 5.625m3.75 0v1.5c0 .621-.504 1.125-1.125 1.125M3.375 4.5c-.621 0-1.125.504-1.125 1.125M3.375 4.5h1.5C5.496 4.5 6 5.004 6 5.625m-3.75 0v1.5c0 .621.504 1.125 1.125 1.125m0 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m1.5-3.75C5.496 8.25 6 7.746 6 7.125v-1.5M4.875 8.25C5.496 8.25 6 8.754 6 9.375v1.5m0-5.25v5.25m0-5.25C6 5.004 6.504 4.5 7.125 4.5h9.75c.621 0 1.125.504 1.125 1.125m1.125 2.625h1.5m-1.5 0A1.125 1.125 0 0 1 18 7.125v-1.5m1.125 2.625c-.621 0-1.125.504-1.125 1.125v1.5m2.625-2.625c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125M18 5.625v5.25M7.125 12h9.75m-9.75 0A1.125 1.125 0 0 1 6 10.875M7.125 12C6.504 12 6 12.504 6 13.125m0-2.25C6 11.496 5.496 12 4.875 12M18 10.875c0 .621-.504 1.125-1.125 1.125M18 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m-12 5.25v-5.25m0 5.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125m-12 0v-1.5c0-.621-.504-1.125-1.125-1.125M18 18.375v-5.25m0 5.25v-1.5c0-.621.504-1.125 1.125-1.125M18 13.125v1.5c0 .621.504 1.125 1.125 1.125M18 13.125c0-.621.504-1.125 1.125-1.125M6 13.125v1.5c0 .621-.504 1.125-1.125 1.125M6 13.125C6 12.504 5.496 12 4.875 12m-1.5 0h1.5m-1.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M19.125 12h1.5m0 0c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h1.5m14.25 0h1.5" />
-              </svg>
-            </div>
+          <Image
+                src={flim}
+                alt="Flim Scanning Images"
+                loading="eager"
+                placeholder="blur"
+                className="m-auto"
+              />
             <div className="text-center w-full text-base font-semibold py-2">Microfilm Scanning</div>
           </div>
         </div>
